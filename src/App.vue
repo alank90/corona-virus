27095f940e6d0c9dd3a2ca179656a1a7a932a255
Alan K <akillian@outlook.com>
2020-03-31 20:37:04 -0400

    commit

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,12 @@
-// ========== /src/App.vue ================== //
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link>
-      <span>&#124;</span>
-      <router-link to="/covid19">Covid-19</router-link>
-      <span>&#124;</span>
-      <router-link to="/johnhopkins">John Hopkins</router-link>
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
     </div>
-    <router-view />
+    <router-view/>
   </div>
 </template>
-
-<script>
-import HelloWorld from "./components/HelloWorld.vue";
-
-export default {
-  name: "App",
-  components: {
-    HelloWorld
-  }
-};
-</script>
 
 <style>
 #app {
@@ -30,11 +15,18 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 
-body {
-  background: url("./assets/img/covid-7-lg.jpg") no-repeat fixed center
-    center/cover;
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
 }
 </style>

e7b066354096d49ea3a4817b78d3bd998cf565d1
Alan Killian <akillian@outlook.com>
2020-09-11 18:28:01 -0400

    commit

--- src/router/index.js
+++ src/router/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 /* jshint esversion:9 */
 /* jshint -W024 */
 
@@ -59,6 +60,7 @@ const routes = [
 
 const router = new VueRouter({
   mode: "history",
+  // eslint-disable-next-line no-undef
   base: process.env.BASE_URL,
   routes,
 });
